bc3d33a005f18df124ccfa3a4207f3fe2b0b5603
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2020-10-05 22:32:03 -0400

    a

--- event_source.js
+++ event_source.js
@@ -4,7 +4,23 @@ if(typeof(EventSource) !== "undefined") {
     });
     source.onmessage = function(event) {
         let data = JSON.parse(event.data);
-        document.getElementById("result").innerHTML += data.nome + "<br>";
+        if (data.monster === 'SIM') {
+            if ( document.getElementById("monster").style.display === 'block') return false;
+            document.getElementById("monster").style.display = 'block';
+            document.getElementById("msg").innerHTML = "<h1>UM MOB APARECEU!</h1>";
+            let audio = new Audio('epic song.mp3');
+            audio.volume = 0.05;
+            audio.play();
+
+            // setTimeout(()=>{
+            //     document.getElementById("monster").style.display = 'none';
+            //     document.getElementById("msg").innerHTML = "";
+            // }, 5000);
+
+        }else{
+            document.getElementById("monster").style.display = 'none';
+            document.getElementById("msg").innerHTML = "";
+        }
     };
     source.addEventListener('teste', function(e) {
         console.warn('ok');
@@ -12,3 +28,8 @@ if(typeof(EventSource) !== "undefined") {
   } else {
     // Sorry! No server-sent events support..
   }
+
+  
+// document.getElementById("monster").style.display = 'block';
+//             document.getElementById("msg").innerHTML = "<h1>UM MOB APARECEU!</h1>";
+//             let audio = new Audio('epic song.mp3');
--- flush.php
+++ flush.php
@@ -2,10 +2,10 @@
     header('Content-Type: text/event-stream');
     header('Cache-Control: no-cache');
 
-    // $time = date('r');
-    // echo "data: The server time is: {$time}\n\n";
     $data = file_get_contents('./mock.json');
     $data = json_decode($data, true);
+
     echo "data: " . json_encode($data) . "\n\n";
+    $valor_atual = $data['monster'];
     flush();
 ?>
