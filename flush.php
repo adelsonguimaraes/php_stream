08a0c5ddd93f77459b23c733e21f36214d8e3df6
Adelson Guimarães <adelsonguimaraes@gmail.com>
2020-10-07 12:14:42 -0400

    atualizando informações de tempo de excução do php, adição de while, e tempo de retry para o js refazer o request

--- flush.php
+++ flush.php
@@ -1,11 +1,23 @@
 <?php
     header('Content-Type: text/event-stream');
     header('Cache-Control: no-cache');
+    header('Connection: keep-alive');
+    header('X-Accel-Buffering: no'); // Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
 
-    $data = file_get_contents('./mock.json');
-    $data = json_decode($data, true);
+    // aumentando o tempo de execução permitido do php
+    set_time_limit( 60 ); // executa por 1 minuto e depois encerra
 
-    echo "data: " . json_encode($data) . "\n\n";
-    $valor_atual = $data['monster'];
-    flush();
+    while(true) {
+        $data = file_get_contents('./mock.json');
+        $data = json_decode($data, true);
+
+        echo "retry: 10000\n";
+        echo "data: " . json_encode($data) . "\n\n";
+        // echo "data: " . date('d/m/Y H:m:s') . "\n\n";
+        $valor_atual = $data['monster'];
+        ob_flush (); 
+        flush ();
+
+        sleep(1);
+    }
 ?>
